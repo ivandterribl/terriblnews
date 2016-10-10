38b99c263f499ca972b24f00308649cc29791d4f
Ivan Regasek <ivan@itweb.co.za>
2016-10-10 15:21:11 +0200

    select-polyfill jshint job responses + 1 bug

--- src/client/app/ui/select-polyfill.js
+++ src/client/app/ui/select-polyfill.js
@@ -3,18 +3,20 @@
 
      var raw = ionic.Platform.navigator.appVersion.match(/Chrom(e|ium)\/([0-9]+)\./),
          chromever = raw ? parseInt(raw[2], 10) : false;
+
      if (chromever >= 53) {
-         function avoidTap() {
-             return {
-                 restrict: 'A',
-                 // scope: '=',
-                 link: (scope, elem) => {
-                     elem.attr('data-tap-disabled', 'true');
-                 },
-             };
-         }
          angular
              .module('ionic')
              .directive('selectPolyfill', avoidTap);
+     }
+
+     function avoidTap() {
+         return {
+             restrict: 'A',
+             // scope: '=',
+             link: function(scope, elem) {
+                 elem.attr('data-tap-disabled', 'true');
+             }
+         };
      }
  })();
